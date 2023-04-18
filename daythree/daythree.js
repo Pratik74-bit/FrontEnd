fdf99272e5e2b9113b95d72bf0ab409e683c8b9d
unknown <you@example.com>
2023-04-18 13:24:48 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,58 @@
+//single line comment
+/*Multiline comment*/
+
+/*
+alert("Congratulations")
+alert("Hello")
+alert(4+2)
+
+// using var
+var x = 15;
+alert(x) // Output: 10
+
+// using let
+let y = "using let";
+alert(y) // Output: "Hello"
+
+// using const
+//const cannot be changed
+const z = 45;
+alert(z) // Output: true
+
+//assignment operator
+let a=12;
+    a*=5;
+alert(a); 
+
+//logical operator
+//AND operator
+let p = 5;
+let s = 10;
+if (p > 0 && s > 0) {
+  alert("Both x and y are positive")
+}
+//OR operator
+let r = 5;
+let f = -10;
+if (r > 0 || f > 0) {
+  alert("Either x or y is positive")
+}
+
+//if else
+if (p>s)
+    alert("p is greater")
+else
+    alert("p is not greater")
+
+//gives datatype
+alert(typeof(true))
+
+
+let age = prompt("What is your name?");
+    alert("Your age is :" + age)
+*/
+
+function Click() {
+    alert("You have successfully inserted image")
+    console.log("You have successfully inserted")
+}
